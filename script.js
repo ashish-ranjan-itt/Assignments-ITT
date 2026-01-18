52cb4141eafee01bfd498b393f44c44640c0860c
Ashish Ranjan <ashish.ranjan@intimetec.com>
2026-01-18 17:26:39 +0530

    assignment 1 ready with details page search and filter functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const jobListing = document.querySelector(".job-portal-job-listings")
 const jobDetailModal = document.querySelector(".job-portal-job-detail-modal")
 const searchElement = document.querySelector(".navigation-bar-search")
+const filterButton = document.querySelector(".job-pertal-filter-icon")
+const filterModal = document.querySelector(".job-filter-modal")
+const closeFilterBtn = document.querySelector(".job-filter-modal-close-btn")
+const filterTypeJobType = document.getElementById("filterType1")
+const filterTypeLocation = document.getElementById("filterType2")
+const filterTypeExperience = document.getElementById("filterType3")
+const addFiltersBtn = document.querySelector(".filter-add-button")
+
+// console.log(filterOptions[0].innerHTML)
 
 let jobs = []
 let searchQuery = ""
@@ -12,28 +21,105 @@ const trimAndAppendString = function (name) {
     return name
 }
 
+const filterJobsByLocation = () => {
+    if (filterTypeLocation.value === "") {
+        return jobs
+    }
+    return jobs.filter((job) => job.location === filterTypeLocation.value)
+}
+
+const filterJobsByJobType = () => {
+    if (filterTypeJobType.value === "") {
+        return jobs
+    }
+    return jobs.filter((job) => job.jobType === filterTypeJobType.value)
+}
+
+const filterJobsByExperience = () => {
+    if (filterTypeExperience.value === "") {
+        return jobs
+    }
+    return jobs.filter((job) =>
+        job.experienceRequired[0] >= filterTypeExperience.value[0] && job.experienceRequired[0] <= filterTypeExperience.value[1]
+    )
+}
+
+const combineAllFilters = (locationFilteredJobs, jobTypeFilteredJobs, experienceFilteredJobs) => {
+    return locationFilteredJobs.filter(job =>
+        jobTypeFilteredJobs.includes(job) &&
+        experienceFilteredJobs.includes(job)
+    );
+}
+
+const applyFilterToJobs = () => {
+    const locationFilteredJobs = filterJobsByLocation()
+    const jobTypeFilteredJobs = filterJobsByJobType()
+    const experienceFilteredJobs = filterJobsByExperience()
+    const combinedFilters = combineAllFilters(locationFilteredJobs, jobTypeFilteredJobs, experienceFilteredJobs)
+    console.log(combinedFilters)
+    return combinedFilters
+}
+
 const openModal = (job) => {
     jobDetailModal.classList.remove("hidden")
     const detailedContent = document.querySelector(".job-detai-modal-content")
-    // detailedContent.innerHTML =
-    // `<div></div>`
+    detailedContent.innerHTML = ""
+    detailedContent.innerHTML = `
+    <div class="job-detail-modal-close-btn">&times;</div>
+                <div class="job-detail-modal-title">Job Application Portal</div>
+                <div class="job-detail-modal-job-title">${job.title}</div>
+                <div class="job-detail-modal-job-location">${job.location}</div>
+                <div class="job-detail-modal-job-description">${job.description}</div>
+                <div class="job-detail-modal-job-apply"><button class="job-detail-modal-job-apply-button"> Apply
+                        Now</button></div>
+                <div class="job-detail-modal-specs">
+                    <div class="job-detail-modal-specs-container">
+                        <div class="job-detail-modal-specs-attribute">
+                            <img src="building.png" class="job-detail-spcs-img"/>
+                            Company Name :<br/> ${job.company}
+                        </div>
+                    </div>
+                    <div class="job-detail-modal-specs-container">
+                        <div class="job-detail-modal-specs-attribute">
+                            <img src="briefcase.png" class="job-detail-spcs-img"/>
+                            Job Type :<br/> ${job.jobType}
+                        </div>
+                    </div>
+                    <div class="job-detail-modal-specs-container">
+                        <div class="job-detail-modal-specs-attribute">
+                            <img src="travel-agency.png" class="job-detail-spcs-img"/>
+                            Experience Required :<br/> ${job.experienceRequired}
+                        </div>
+                    </div>
+                </div>
+    `
+    const closeJobDetailModal = detailedContent.querySelector(".job-detail-modal-close-btn")
+    closeJobDetailModal.addEventListener("click", function () {
+        closeModal()
+    })
 }
 
-filteredData = () => {  
+filteredData = () => {
     const query = searchQuery.toLowerCase()
     const applyFilter = jobs.filter((job) => {
-        return job.title.toLowerCase().includes(query)||
-        job.location.toLowerCase().includes(query) ||
-        job.company.toLowerCase().includes(query) ||
-        job.jobType?.toLowerCase().includes(query)
+        return job.title.toLowerCase().includes(query) ||
+            job.location.toLowerCase().includes(query) ||
+            job.company.toLowerCase().includes(query) ||
+            job.jobType?.toLowerCase().includes(query)
     })
     // console.log(applyFilter)
     return applyFilter
 }
 
-const renderJobs = (jobs) => {
+const clearFilters = () => {
+  filterTypeJobType.selectedIndex = 0;
+  filterTypeLocation.selectedIndex = 0;
+  filterTypeExperience.selectedIndex = 0;
+}
+
+const renderSearchedJobs = (jobs) => {
     jobListing.innerHTML = ""
-    if(jobs.length === 0) {
+    if (jobs.length === 0) {
         jobListing.innerHTML = `<div class="no-jobs-container">
         <img src="image3.png" class="no-jobs-illustration"/>
         No jobs available for this keyword
@@ -103,6 +189,50 @@ searchElement.addEventListener("input", (e) => {
     searchQuery = e.target.value
     // console.log(searchQuery)
     const filteredJobs = filteredData()
-    renderJobs(filteredJobs)
+    renderSearchedJobs(filteredJobs)
 })
+
+filterButton.addEventListener("click", () => {
+    filterModal.classList.remove("hideFilters")
+})
+
+closeFilterBtn.addEventListener("click", () => {
+    filterModal.classList.add("hideFilters")
+})
+
+addFiltersBtn.addEventListener("click", () => {
+    // console.log(filterTypeJobType.value,filterTypeExperience.value,filterTypeLocation.value)
+    const filteredJobs = applyFilterToJobs()
+    filterModal.classList.add("hideFilters")
+    clearFilters();
+    renderSearchedJobs(filteredJobs)
+})
+
+// filterTypeJobType.addEventListener("change", (e) => {
+//   console.log("Selected job type:", e.target.value);
+// });
+
+// filterTypeExperience.addEventListener("change", (e) => {
+//   console.log("Selected job type:", e.target.value);
+// });
+
+// filterTypeLocation.addEventListener("change", (e) => {
+//   console.log("Selected job type:", e.target.value);
+// });
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && !filterModal.classList.contains("hideFilters")) {
+        filterModal.classList.add("hideFilters")
+    }
+})
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && !jobDetailModal.classList.contains("hidden")) {
+        jobDetailModal.classList.add("hidden")
+    }
+})
+
+const closeModal = () => {
+    jobDetailModal.classList.add("hidden")
+}
 
